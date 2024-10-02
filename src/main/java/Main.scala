63036f65de42117bc3559a40bcc6324babc90248
CafeSua29 <luongbaphananh@gmail.com>
2024-10-02 15:55:28 +0700

    38

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -107,126 +107,126 @@ object Main {
       .parquet("hdfs://namenode:9000/Phan4/Bai4")
   }
 
-  private def readHDFSThenPutToHBase(): Unit = {
-    var df = spark.read.parquet("hdfs://namenode:9000/Phan4/Bai4")
-    df.printSchema()
-
-    df = df.withColumn("day", date_format(col("timeCreate"), "yyyy-MM-dd"))
-
-    def catalog =
-      s"""{
-         |"table":{"namespace":"pageviewlog", "name":"pageviewlog_info"},
-         |"rowkey":"guid",
-         |"columns":{
-         |"guid":{"cf":"rowkey", "col":"guid", "type":"long"},
-         |"timeCreate":{"cf":"consumer", "col":"timeCreate", "type":"string"},
-         |"cookieCreate":{"cf":"consumer", "col":"cookieCreate", "type":"string"},
-         |"browserCode":{"cf":"hardware", "col":"browserCode", "type":"int"},
-         |"browserVer":{"cf":"hardware", "col":"browserVer", "type":"string"},
-         |"osCode":{"cf":"hardware", "col":"osCode", "type":"int"},
-         |"osVer":{"cf":"hardware", "col":"osVer", "type":"string"},
-         |"ip":{"cf":"consumer", "col":"ip", "type":"long"},
-         |"locId":{"cf":"consumer", "col":"locId", "type":"int"},
-         |"domain":{"cf":"producer", "col":"domain", "type":"string"},
-         |"siteId":{"cf":"producer", "col":"siteId", "type":"int"},
-         |"cId":{"cf":"consumer", "col":"cId", "type":"int"},
-         |"path":{"cf":"producer", "col":"path", "type":"string"},
-         |"referer":{"cf":"producer", "col":"referer", "type":"string"},
-         |"flashVersion":{"cf":"hardware", "col":"flashVersion", "type":"string"},
-         |"jre":{"cf":"hardware", "col":"jre", "type":"string"},
-         |"sr":{"cf":"hardware", "col":"sr", "type":"string"},
-         |"sc":{"cf":"hardware", "col":"sc", "type":"string"},
-         |"geographic":{"cf":"consumer", "col":"geographic", "type":"int"},
-         |"url":{"cf":"producer", "col":"url", "type":"string"},
-         |"category":{"cf":"producer", "col":"category", "type":"string"},
-         |"day":{"cf":"consumer", "col":"day", "type":"string"}
-         |}
-         |}""".stripMargin
-
-    df.write
-      .options(Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5"))
-      .format("org.apache.hadoop.hbase.spark")
-      .save()
-  }
-
 //  private def readHDFSThenPutToHBase(): Unit = {
 //    var df = spark.read.parquet("hdfs://namenode:9000/Phan4/Bai4")
 //    df.printSchema()
 //
-//    df = df
-//      .withColumn("day", date_format(col("timeCreate"), "yyyy-MM-dd"))
-//      .repartition(5)
+//    df = df.withColumn("day", date_format(col("timeCreate"), "yyyy-MM-dd"))
 //
-//    val batchPutSize = 100
+//    def catalog =
+//      s"""{
+//         |"table":{"namespace":"pageviewlog", "name":"pageviewlog_info"},
+//         |"rowkey":"guid",
+//         |"columns":{
+//         |"guid":{"cf":"rowkey", "col":"guid", "type":"long"},
+//         |"timeCreate":{"cf":"consumer", "col":"timeCreate", "type":"string"},
+//         |"cookieCreate":{"cf":"consumer", "col":"cookieCreate", "type":"string"},
+//         |"browserCode":{"cf":"hardware", "col":"browserCode", "type":"int"},
+//         |"browserVer":{"cf":"hardware", "col":"browserVer", "type":"string"},
+//         |"osCode":{"cf":"hardware", "col":"osCode", "type":"int"},
+//         |"osVer":{"cf":"hardware", "col":"osVer", "type":"string"},
+//         |"ip":{"cf":"consumer", "col":"ip", "type":"long"},
+//         |"locId":{"cf":"consumer", "col":"locId", "type":"int"},
+//         |"domain":{"cf":"producer", "col":"domain", "type":"string"},
+//         |"siteId":{"cf":"producer", "col":"siteId", "type":"int"},
+//         |"cId":{"cf":"consumer", "col":"cId", "type":"int"},
+//         |"path":{"cf":"producer", "col":"path", "type":"string"},
+//         |"referer":{"cf":"producer", "col":"referer", "type":"string"},
+//         |"flashVersion":{"cf":"hardware", "col":"flashVersion", "type":"string"},
+//         |"jre":{"cf":"hardware", "col":"jre", "type":"string"},
+//         |"sr":{"cf":"hardware", "col":"sr", "type":"string"},
+//         |"sc":{"cf":"hardware", "col":"sc", "type":"string"},
+//         |"geographic":{"cf":"consumer", "col":"geographic", "type":"int"},
+//         |"url":{"cf":"producer", "col":"url", "type":"string"},
+//         |"category":{"cf":"producer", "col":"category", "type":"string"},
+//         |"day":{"cf":"consumer", "col":"day", "type":"string"}
+//         |}
+//         |}""".stripMargin
 //
-//    df.foreachPartition((rows: Iterator[Row]) => {
-//      val hbaseConnection = HBaseConnectionFactory.createConnection()
-//      //val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-//
-//      try {
-//        val table = hbaseConnection.getTable(TableName.valueOf("pageviewlog"))
-//        val puts = new util.ArrayList[Put]()
-//        for (row <- rows) {
-//          val timeCreate = row.getAs[Timestamp]("timeCreate").toString
-//          val cookieCreate = row.getAs[Timestamp]("cookieCreate").toString
-//          val browserCode = row.getAs[Int]("browserCode")
-//          val browserVer = row.getAs[String]("browserVer")
-//          val osCode = row.getAs[Int]("osCode")
-//          val osVer = row.getAs[String]("osVer")
-//          val ip = row.getAs[Long]("ip")
-//          val locId = row.getAs[Int]("locId")
-//          val domain = row.getAs[String]("domain")
-//          val siteId = row.getAs[Int]("siteId")
-//          val cId = row.getAs[Int]("cId")
-//          val path = row.getAs[String]("path")
-//          val referer = row.getAs[String]("referer")
-//          val guid = row.getAs[Long]("guid")
-//          val flashVersion = row.getAs[String]("flashVersion")
-//          val jre = row.getAs[String]("jre")
-//          val sr = row.getAs[String]("sr")
-//          val sc = row.getAs[String]("sc")
-//          val geographic = row.getAs[Int]("geographic")
-//          val url = row.getAs[String]("url")
-//          val category = row.getAs[String]("category")
-//          val day = row.getAs[String]("day")
-//
-//          val put = new Put(Bytes.toBytes(guid))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("timeCreate"), Bytes.toBytes(timeCreate))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("cookieCreate"), Bytes.toBytes(cookieCreate))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("browserCode"), Bytes.toBytes(browserCode))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("browserVer"), Bytes.toBytes(browserVer))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("osCode"), Bytes.toBytes(osCode))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("osVer"), Bytes.toBytes(osVer))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("ip"), Bytes.toBytes(ip))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("locId"), Bytes.toBytes(locId))
-//          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("domain"), Bytes.toBytes(domain))
-//          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("siteId"), Bytes.toBytes(siteId))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("cId"), Bytes.toBytes(cId))
-//          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("path"), Bytes.toBytes(path))
-//          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("referer"), Bytes.toBytes(referer))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("flashVersion"), Bytes.toBytes(flashVersion))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("jre"), Bytes.toBytes(jre))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("sr"), Bytes.toBytes(sr))
-//          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("sc"), Bytes.toBytes(sc))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("geographic"), Bytes.toBytes(geographic))
-//          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("url"), Bytes.toBytes(url))
-//          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("category"), Bytes.toBytes(category))
-//          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("day"), Bytes.toBytes(day))
-//
-//          puts.add(put)
-//          if (puts.size > batchPutSize) {
-//            table.put(puts)
-//            puts.clear()
-//          }
-//        }
-//        if (puts.size() > 0) {
-//          table.put(puts)
-//        }
-//      } finally {
-//        hbaseConnection.close()
-//      }
-//    })
+//    df.write
+//      .options(Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5"))
+//      .format("org.apache.hadoop.hbase.spark")
+//      .save()
 //  }
+
+  private def readHDFSThenPutToHBase(): Unit = {
+    var df = spark.read.parquet("hdfs://namenode:9000/Phan4/Bai4")
+    df.printSchema()
+
+    df = df
+      .withColumn("day", date_format(col("timeCreate"), "yyyy-MM-dd"))
+      .repartition(5)
+
+    val batchPutSize = 100
+
+    df.foreachPartition((rows: Iterator[Row]) => {
+      val hbaseConnection = HBaseConnectionFactory.createConnection()
+      //val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+      try {
+        val table = hbaseConnection.getTable(TableName.valueOf("pageviewlog"))
+        val puts = new util.ArrayList[Put]()
+        for (row <- rows) {
+          val timeCreate = row.getAs[Timestamp]("timeCreate").toString
+          val cookieCreate = row.getAs[Timestamp]("cookieCreate").toString
+          val browserCode = row.getAs[Int]("browserCode")
+          val browserVer = row.getAs[String]("browserVer")
+          val osCode = row.getAs[Int]("osCode")
+          val osVer = row.getAs[String]("osVer")
+          val ip = row.getAs[Long]("ip")
+          val locId = row.getAs[Int]("locId")
+          val domain = row.getAs[String]("domain")
+          val siteId = row.getAs[Int]("siteId")
+          val cId = row.getAs[Int]("cId")
+          val path = row.getAs[String]("path")
+          val referer = row.getAs[String]("referer")
+          val guid = row.getAs[Long]("guid")
+          val flashVersion = row.getAs[String]("flashVersion")
+          val jre = row.getAs[String]("jre")
+          val sr = row.getAs[String]("sr")
+          val sc = row.getAs[String]("sc")
+          val geographic = row.getAs[Int]("geographic")
+          val url = row.getAs[String]("url")
+          val category = row.getAs[String]("category")
+          val day = row.getAs[String]("day")
+
+          val put = new Put(Bytes.toBytes(guid))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("timeCreate"), Bytes.toBytes(timeCreate))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("cookieCreate"), Bytes.toBytes(cookieCreate))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("browserCode"), Bytes.toBytes(browserCode))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("browserVer"), Bytes.toBytes(browserVer))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("osCode"), Bytes.toBytes(osCode))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("osVer"), Bytes.toBytes(osVer))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("ip"), Bytes.toBytes(ip))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("locId"), Bytes.toBytes(locId))
+          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("domain"), Bytes.toBytes(domain))
+          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("siteId"), Bytes.toBytes(siteId))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("cId"), Bytes.toBytes(cId))
+          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("path"), Bytes.toBytes(path))
+          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("referer"), Bytes.toBytes(referer))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("flashVersion"), Bytes.toBytes(flashVersion))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("jre"), Bytes.toBytes(jre))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("sr"), Bytes.toBytes(sr))
+          put.addColumn(Bytes.toBytes("hardware"), Bytes.toBytes("sc"), Bytes.toBytes(sc))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("geographic"), Bytes.toBytes(geographic))
+          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("url"), Bytes.toBytes(url))
+          put.addColumn(Bytes.toBytes("producer"), Bytes.toBytes("category"), Bytes.toBytes(category))
+          put.addColumn(Bytes.toBytes("consumer"), Bytes.toBytes("day"), Bytes.toBytes(day))
+
+          puts.add(put)
+          if (puts.size > batchPutSize) {
+            table.put(puts)
+            puts.clear()
+          }
+        }
+        if (puts.size() > 0) {
+          table.put(puts)
+        }
+      } finally {
+        hbaseConnection.close()
+      }
+    })
+  }
 
   def main(args: Array[String]): Unit = {
 //    val conf = HBaseConfiguration.create()
